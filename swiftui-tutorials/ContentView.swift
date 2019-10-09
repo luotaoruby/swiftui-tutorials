489db016c96c43abcc84b3200c95b45e2d5697a3
luotao <luotao.ruby@gmail.com>
2019-10-09 19:23:27 +0800

    Initial Commit

---
+++ swiftui-tutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-tutorials
+//
+//  Created by luotao on 2019/10/9.
+//  Copyright © 2019 luotao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
